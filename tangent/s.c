735f6975f8443d20fbf8b9f85d2e7043fd2e874e
electronoob <electronoob@electronoob.com>
2018-01-19 22:16:53 +0000

    struct with funcs and 'this'.

--- tangent/s.c
+++ tangent/s.c
@@ -3,17 +3,6 @@
 
 char string[] = "; 16-bit Integer to ASCII (decimal)\n; http://map.grauw.nl/sources/external/z80bits.html#5.1\n;\n; Input: HL = number to convert, DE = location of ASCII string\n; Output: ASCII string at (DE)\n\n\nNum2Dec:\n\tld\tbc,-10000\n\tcall\tNum1\n\tld\tbc,-1000\n\tcall\tNum1\n\tld\tbc,-100\n\tcall\tNum1\n\tld\tc,-10\n\tcall\tNum1\n\tld\tc,b\n\nNum1:\t\n\tld\ta,'0'-1\nNum2:\t\n\tinc\ta\n\tadd\thl,bc\n\tjr\tc,Num2\n\tsbc\thl,bc\n\n\tld\t[de],a\n\tinc\tde\n\tret\n";
 int string_len;
-int next()
-{
-	printf("next\n");
-	return -1;
-}
-int prev()
-{
-	printf("prev\n");
-	return 1;
-}
-
 
 struct parser_struct
 {
@@ -28,9 +17,22 @@ typedef struct parser_struct parser;
 
 parser p = {};
 
+int next(parser this)
+{
+	printf("next %i\n", this.c_offset);
+	return -1;
+}
+
+int prev(parser this)
+{
+	printf("prev\n");
+	return 1;
+}
+
 int init()
 {
 	p.next = &next;
+	p.prev = &prev;
 	return 0;
 }
 
@@ -38,7 +40,7 @@ int main()
 {
 	init();
 	string_len = strlen(string);
-	p.next();
+	p.next(p);
 	return 0;
 }
 
