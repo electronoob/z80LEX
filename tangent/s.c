c9cbf870bfc34f72789bedf5cf0a1f5ae392bc31
electronoob <electronoob@electronoob.com>
2018-01-20 01:35:25 +0000

    yay! tokenizer working in forward direction.

--- tangent/s.c
+++ tangent/s.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
 char string[] = "; 16-bit Integer to ASCII (decimal)\n; http://map.grauw.nl/sources/external/z80bits.html#5.1\n;\n; Input: HL = number to convert, DE = location of ASCII string\n; Output: ASCII string at (DE)\n\n\nNum2Dec:\n\tld\tbc,-10000\n\tcall\tNum1\n\tld\tbc,-1000\n\tcall\tNum1\n\tld\tbc,-100\n\tcall\tNum1\n\tld\tc,-10\n\tcall\tNum1\n\tld\tc,b\n\nNum1:\t\n\tld\ta,'0'-1\nNum2:\t\n\tinc\ta\n\tadd\thl,bc\n\tjr\tc,Num2\n\tsbc\thl,bc\n\n\tld\t[de],a\n\tinc\tde\n\tret\n";
 int string_len;
-
 struct parser_struct
 {
-	int c_line;
-	int c_char;
-	int c_offset;
+	int x;
+	int y;
+	int o;
 	int (*next)();
 	int (*prev)();
 	int (*bookmark)();
@@ -19,20 +17,42 @@ parser p = {};
 
 int next(parser this)
 {
-	printf("next %i\n", this.c_offset);
-	return -1;
+	char whitespace[] = " \t\n";
+	int ws_len = 3,i,k;
+	uint8_t *a, *b;
+
+	for(i=p.o; i<string_len;i++)
+	{
+		a = string[i];
+		for(k=0; k<ws_len;k++)
+		{
+			b = whitespace[k];
+			if(a == b)
+			{
+				p.o = i+1;
+				return 1;
+			}
+
+		}
+	}
+	return 0;
 }
 
 int prev(parser this)
 {
 	printf("prev\n");
-	return 1;
+	return 0;
 }
-
+int bookmark(parser this)
+{
+	printf("bookmark\n");
+	return 0;
+}
 int init()
 {
 	p.next = &next;
 	p.prev = &prev;
+	p.bookmark = &bookmark;
 	return 0;
 }
 
@@ -40,7 +60,22 @@ int main()
 {
 	init();
 	string_len = strlen(string);
-	p.next(p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
+	printf("[%c]", string[p.o]);
+	p.next(&p);
 	return 0;
 }
 
